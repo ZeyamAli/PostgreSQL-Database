a5c434104c0ed04f69d0262bd0ef503c4900eed7
Zeyam Ali <zeyamalimatharu@gmail.com>
2024-10-20 06:56:46 -0700

    text fields and button created

--- student.py
+++ student.py
@@ -11,4 +11,27 @@ frame.place(relheight=.8,relwidth=.8, relx=0.3,rely=0.1)
 label = Label(frame, text="Data Added")
 label.grid(row=0, column=1)
 
+label = Label(frame, text="Name")
+label.grid(row=2,column=0)
+
+entryName = Entry(frame)
+entryName.grid(row=2, column=1)
+
+label = Label(frame, text="Age")
+label.grid(row=4,column=0)
+
+entryAge = Entry(frame)
+entryAge.grid(row=4, column=1)
+
+label = Label(frame, text="Address")
+label.grid(row=6,column=0)
+
+entryAddress = Entry(frame)
+entryAddress.grid(row=6, column=1)
+
+button = Button(frame, text="Add")
+button.grid(row=8, column=2)
+
+
+
 root.mainloop()
