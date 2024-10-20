1e8df7f01834c2ec17145df19a8729c90e729c44
Zeyam Ali <zeyamalimatharu@gmail.com>
2024-10-19 18:41:58 -0700

    User given data successfully inserted

--- demo.py
+++ demo.py
@@ -1,1 +1,22 @@
-print("My name is Zeyam Ali")
+import pg8000
+
+# Database connection
+conn = pg8000.connect(database="postgres", user="postgres", password="123", host="localhost", port="5432")
+cur = conn.cursor()
+
+# Get input from the user
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+address = input("Please enter your address: ")
+
+# Correct query and execution
+query = '''INSERT INTO students (name, age, address) VALUES (%s, %s, %s);'''
+try:
+    cur.execute(query, (name, age, address))
+    conn.commit()  # Commit the transaction
+    print("Data inserted successfully!")
+except pg8000.ProgrammingError as e:
+    print("Error: ", e)
+finally:
+    cur.close()
+    conn.close()
